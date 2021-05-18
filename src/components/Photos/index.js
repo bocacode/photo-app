96e4779f83629ff3832697ed2d9b0610d9d7874e
Todd Albert <todd@bocacode.com>
2021-05-18 15:20:02 -0400

    save file

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,22 +1,26 @@
 import React, { useState, useCallback } from 'react'
 import Cropper from 'react-easy-crop'
 import { getOrientation } from 'get-orientation/browser'
-import { readFile, getRotatedImage } from '../../utils/images'
+import { readFile, getRotatedImage, getCroppedImg } from '../../utils/images'
 
 const ORIENTATION_TO_ANGLE = {
   '3': 180,
   '6': 90,
-  '8': -90,
+  '8': -90
 }
 
-function Photo() {
+function Photo () {
   const [imageSrc, setImageSrc] = useState(null)
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+  const [croppedImage, setCroppedImage] = useState(null)
+  const [fileName, setFileName] = useState(null)
 
-  const onFileChange = async (e) => {
+  const onFileChange = async e => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0]
+      setFileName(file.path)
       let imageDataUrl = await readFile(file)
       // apply rotation if needed
       const orientation = await getOrientation(file)
@@ -28,19 +32,41 @@ function Photo() {
     }
   }
 
+  const onCropComplete = useCallback((croppedArea, newCroppedAreaPixels) => {
+    setCroppedAreaPixels(newCroppedAreaPixels)
+  }, [])
+
+  const saveCroppedImage = useCallback(async () => {
+    try {
+      const newCroppedImage = await getCroppedImg(
+        fileName,
+        imageSrc,
+        croppedAreaPixels
+      )
+      console.log('done', { newCroppedImage })
+      setCroppedImage(newCroppedImage)
+    } catch (e) {
+      console.error(e)
+    }
+  }, [croppedAreaPixels])
+
+  if (!imageSrc) {
+    return <input type='file' accept='image/*' onChange={onFileChange} />
+  }
   return (
     <div>
-    {!imageSrc
-      ? <input type="file" accept="image/*" onChange={onFileChange} />
-      : <Cropper
-          crop={crop}
-          zoom={zoom}
-          onCropChange={setCrop}
-          onZoomChange={setZoom}
-          image={imageSrc}
-          aspect={4/3}
-        />
-    }
+      <Cropper
+        crop={crop}
+        zoom={zoom}
+        onCropChange={setCrop}
+        onZoomChange={setZoom}
+        onCropComplete={onCropComplete}
+        image={imageSrc}
+        aspect={4 / 3}
+      />
+      <button className='save-btn' onClick={saveCroppedImage}>
+        Save
+      </button>
     </div>
   )
 }
